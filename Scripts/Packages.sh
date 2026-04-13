04d5ff1a1421430fb065b15521112be6d53bcd55
simple <2424585654@qq.com>
2026-04-14 00:15:37 +0800

    优化2

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -54,7 +54,7 @@ UPDATE_PACKAGE() {
 # UPDATE_PACKAGE "nikki" "nikkinikki-org/OpenWrt-nikki" "main"
 # UPDATE_PACKAGE "momo" "nikkinikki-org/OpenWrt-momo" "main"
 # UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
-UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "main" "" "luci-app-openclash"
+UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "master"
 # UPDATE_PACKAGE "passwall" "Openwrt-Passwall/openwrt-passwall" "main" "pkg"
 # UPDATE_PACKAGE "passwall2" "Openwrt-Passwall/openwrt-passwall2" "main" "pkg"
 # UPDATE_PACKAGE "alist" "sbwml/luci-app-alist" "main"
